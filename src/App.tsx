f515e68985e8c17c854ec6f512a3680a0d7f2a02
cam <cwan0148@student.monash.edu>
2023-05-23 13:43:20 +1000

    add mui deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import { Container } from '@mui/material'
 
 function App() {
 
   return (
     <>
-     
+     <Container maxWidth="lg">
+        <h1>React TypeScript Template</h1>
+     </Container>
     </>
   )
 }
