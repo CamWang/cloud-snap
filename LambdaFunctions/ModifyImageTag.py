d868a50f17e36415a9e12f9ad246eee37b3aae6f
ryan0049 <ryan0049@student.monash.edu>
2023-06-02 15:55:19 +1000

    little changes

--- LambdaFunctions/ModifyImageTag.py
+++ LambdaFunctions/ModifyImageTag.py
@@ -1,4 +1,5 @@
 import boto3
+import json
 
 def get_target_item(items, item_key):
     for item in items:
@@ -9,15 +10,18 @@ def get_target_item(items, item_key):
 
 def modify_item(modify_type, target_item, new_tag_list):
     if int(modify_type) == 1:
-        for old_tag in target_item["tags"]["L"]:
-            for new_tag in new_tag_list:
+        for new_tag in new_tag_list:
+            for old_tag in target_item["tags"]["L"]:
                 if new_tag["tag"] == old_tag["M"]["tag"]["S"]:
-                    if hasattr(new_tag, "count"):
-                        old_tag["M"]["count"]["N"] = int(old_tag["M"]["count"]["N"]) + int(new_tag["count"])
+                    if "count" in new_tag:
+                        old_tag["M"]["count"]["N"] = str(int(old_tag["M"]["count"]["N"]) + int(new_tag["count"]))
                     else:
                         new_tag["count"] == 1
-                        old_tag["M"]["count"]["N"] = int(old_tag["M"]["count"]["N"]) + 1
+                        old_tag["M"]["count"]["N"] = str(int(old_tag["M"]["count"]["N"]) + 1)
                     return target_item
+                else:
+
+
     
     if int(modify_type) == 0:
         updated_tag_list = []
@@ -25,9 +29,9 @@ def modify_item(modify_type, target_item, new_tag_list):
             for new_tag in new_tag_list:
                 if new_tag["tag"] == old_tag["M"]["tag"]["S"]:
                     if int(old_tag["M"]["count"]["N"]) > int(new_tag["count"]):
-                        old_tag["M"]["count"]["N"] = int(old_tag["M"]["count"]["N"]) - int(new_tag["count"])
+                        old_tag["M"]["count"]["N"] = str(int(old_tag["M"]["count"]["N"]) - int(new_tag["count"]))
                     else:
-                        old_tag["M"]["count"]["N"] = 0
+                        old_tag["M"]["count"]["N"] = '0'
 
         for old_tag in target_item["tags"]["L"]:
             if int(old_tag["M"]["count"]["N"]) != 0:
@@ -38,17 +42,19 @@ def modify_item(modify_type, target_item, new_tag_list):
 
 
 def lambda_handler(event, context):
+    print(json.dumps(event))
     modify_image_key = event["key"]
     modify_type = event["type"]
     new_tag_list = event["tags"]
 
 
-    dynamoDB = boto3.client("dynamoDB")
+    dynamoDB = boto3.client("dynamodb")
     response = dynamoDB.scan(TableName = "image_tags")
     items = response["Items"]
 
     target_item = get_target_item(items, modify_image_key)
     new_item = modify_item(modify_type, target_item, new_tag_list)
+    print(json.dumps(new_item))
     dynamoDB.put_item(
         TableName = "image_tags",
         Item = new_item
